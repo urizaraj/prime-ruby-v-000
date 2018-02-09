1984ad5a98b83d1d5261e6f47e96c5e3843c95d2
Alexander Urizar <urizaraj@gmail.com>
2018-02-09 17:42:27 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -8,13 +8,13 @@ def prime?(n)
   elsif n % 2 == 0
     return false
   end
-    
+
   i = 5
   w = 2
-  
+
   while i * i <= n
     return false if n % i == 0
-    
+
     i += w
     w = 6 - w
   end
